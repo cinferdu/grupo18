93f66aa2852728b7a4aa77fb0c6d39ee69f86a51
Cinthya Fernandez <cinferdu@hotmail.com>
2019-09-19 19:17:29 -0300

    TEst con @Before

--- TPComplejidadComputacional/src/tpcc/PolinimioTest.java
+++ TPComplejidadComputacional/src/tpcc/PolinimioTest.java
@@ -1,40 +1,32 @@
 package tpcc;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 class PolinimioTest {
-	
-	
-	
-
-	private final int POL_1_GRADO = 100000;
+	private final int POL_1_GRADO = 10000000;
 	private final int I = POL_1_GRADO + 1;
 	private final int POL_1_VAL_X = 0;
 	private final double POL_1_RESULTADO = 0;
+	private double[] coef = new double[I];
 
-	@Test
-	void testSucesivo() {
-		double[] coef = new double[I];
-
+	@Before
+	void crearPrueba() {
 		for (int i = 0; i < I; i++) {
 			coef[i] = 0;
 		}
+	}
 
+	@Test
+	void testSucesivo() {
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarMSucesivas(POL_1_VAL_X), 0.00);
 	}
 
 	@Test
 	void testRecursivo() {
-		double[] coef = new double[I];
-
-		for (int i = 0; i < I; i++) {
-			coef[i] = 0;
-		}
-		
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarRecursiva(POL_1_VAL_X), 0.00);
 	}
@@ -46,52 +38,31 @@ class PolinimioTest {
 		for (int i = 0; i < I; i++) {
 			coef[i] = 0;
 		}
-		
+
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarRecursivaPar(POL_1_VAL_X), 0.00);
 	}
 
 	@Test
 	void evaluarProgDinamicaTest() {
-		double[] coef = new double[I];
-
-		for (int i = 0; i < I; i++) {
-			coef[i] = 0;
-		}
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarProgDinamica(POL_1_VAL_X), 0.00);
 	}
 
 	@Test
 	void evaluarMejoradaTest() {
-		double[] coef = new double[I];
-
-		for (int i = 0; i < I; i++) {
-			coef[i] = 0;
-		}
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarMejorada(POL_1_VAL_X), 0.00);
 	}
 
 	@Test
 	void evaluarPowTest() {
-		double[] coef = new double[I];
-
-		for (int i = 0; i < I; i++) {
-			coef[i] = 0;
-		}
-		
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarPow(POL_1_VAL_X), 0.00);
 	}
 
 	@Test
 	void evaluarHornerTest() {
-		double[] coef = new double[I];
-
-		for (int i = 0; i < I; i++) {
-			coef[i] = 0;
-		}
 		Polinomio p1 = new Polinomio(POL_1_GRADO, coef);
 		Assert.assertEquals(POL_1_RESULTADO, p1.evaluarHorner(POL_1_VAL_X), 0.00);
 	}
